53b815245698451c79b8d31e2fdfbb1d09e607ee
Kevin Oviedo <kevinoviedo2018@gmail.com>
2023-01-30 20:06:41 -0300

    agrego lista de card

--- src/components/BarraNavegacion/ItemListContainer/ItemListContainer.jsx
+++ src/components/BarraNavegacion/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { gFetch } from "../../utils/gFetch";
+
+
 
 
  const ItemListContainer = ({saludo}) => {
+    const [productos, setProducto] = useState([]);
+
+    useEffect(() => {
+    gFetch()
+     .then(res=> {
+        setProducto(res)
+     })
+     .catch(err =>  console.log(err) )
+    }, [])
+
+   
+    console.log(productos);
+
     return(
-        <h2 style={{textAlign: 'center'}}>
-            {saludo}
-        </h2>
+        <ul>
+           {productos.map(producto => 
+           <div key={producto.id} className="card w-25 mt-2">
+            <div className="card-header">
+               Nombre: {producto.name} 
+            </div>
+            <div className="card-body">
+               <img src={producto.foto} alt="foto" className="w-100" />
+            </div>
+            <div className="card-footer">
+                Categoria: {producto.categoria}<br/>
+                Price: {producto.price}
+                <button className="btn btn-outline-primary w-100">Detalle</button>
+            </div>
+           </div>)}
+            
+        </ul>
     );
 }
 
